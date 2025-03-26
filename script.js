0fcaf9aac0af665b6aca7ada98401ccb9fc984fe
vpppv <v.tipeu@gmail.com>
2025-03-26 22:17:59 +0100

    Update script.js Retour aux bases #2

--- script.js
+++ script.js
@@ -28,7 +28,11 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     miniGamesList.appendChild(gameElement);
-  });
+ // ✅ Clique pour masquer la règle
+gameElement.addEventListener('click', () => {
+  gameElement.classList.add('clicked');
+});
+
 
   const boardCells = document.querySelectorAll('.board-cell');
 
